799a453e31eed3ce7c5ec46e9594b4f1622552ea
MarioLatifi <148716635+MarioLatifi@users.noreply.github.com>
2025-01-12 17:06:15 +0100

    wpf con tutto l'essenziale per ora, e boh, figo per essere la mia prima wpf

---
+++ EsempioWpf/Ristorante.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EsempioWpf
+{
+    public class Ristorante
+    {
+        private string _nome;
+        public string Nome { 
+            get { return _nome; } 
+            set { _nome = value; }
+        }
+        public int Tavoli;
+        public int[] PersoneNeiTavoli;
+        public Ristorante(string nome, int tavoli)
+        {
+            Nome = nome;
+            if (tavoli < 0) throw new ArgumentOutOfRangeException();
+            Tavoli = tavoli;
+            PersoneNeiTavoli = new int[tavoli];
+        }
+        public bool Prenota(string nome, int persone)
+        {
+            if(persone < 0||persone>4) throw new ArgumentOutOfRangeException("troppe o troppo poche persone");
+            if (string.IsNullOrEmpty(nome)) throw new ArgumentNullException("inserire un nome valido");
+            for (int i = 0; i < PersoneNeiTavoli.Length; i++)
+            {
+                if (PersoneNeiTavoli[i] == 0) 
+                {
+                    PersoneNeiTavoli [i] = persone;
+                    return true;
+                }
+            }
+            return false;
+        }
+        /*
+         * numero di tavoli prenotati
+           numero di tavoli liberi
+           numero di coperti ancora disponibili
+         */
+        public int TavoliPrenotati()
+        {
+            int counter = 0;
+            for (int i = 0; i < PersoneNeiTavoli.Length; i++)
+            {
+                if (PersoneNeiTavoli[i] != 0)
+                {
+                    counter++;
+                }
+            }
+            return counter;
+        }
+        public int TavoliLiberi
+        {
+            get
+            {
+                return(Tavoli-TavoliPrenotati());
+            }
+        }
+        public int CopertiDisponibili()
+        {
+            int counter = 0;
+            for (int i = 0; i < PersoneNeiTavoli.Length; i++)
+            {
+                counter += 4-PersoneNeiTavoli[i];
+            }
+            return counter;
+        }
+        public override string ToString()
+        {
+            return _nome.ToUpper() + " numero massimo coperti: " + Tavoli;
+        }
+    }
+}
---
+++ EsempioWpf/RistoranteWpf.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace EsempioWpf
+{
+    /// <summary>
+    /// Logica di interazione per Ristorante.xaml
+    /// </summary>
+    public partial class RistoranteWpf : Window
+    {
+        Ristorante Ristorante;
+        public RistoranteWpf(Ristorante r)
+        {
+            Ristorante = r;
+            InitializeComponent();
+            lblNomeRisto.Content = "Il nome del ristorante é: " + Ristorante;
+            lblCopertiLiberi.Content = "Il numero di coperti disponibili é: " + Ristorante.CopertiDisponibili();
+            lblTavoliLiberi.Content = "Tavoli Liberi: " + Ristorante.TavoliLiberi;
+            lblTavoliPrenotati.Content = "Tavoli prenotati : " + Ristorante.TavoliPrenotati();
+        }
+
+        private void ClickPrenota(object sender, RoutedEventArgs e)
+        {
+            if(sender is Button) {
+                try
+                {
+                    int numPersone = int.Parse(txtNumClienti.Text.ToString());
+                    if (Ristorante.Prenota(txtNomeCliente.Text.ToString(), numPersone))//se riesco a prenotare
+                    {
+                        lblStato.Content = "Prenotazione\navvenuta con Successo!";
+                        InitializeComponent();
+                        lblNomeRisto.Content = "Il nome del ristorante é: " + Ristorante;
+                        lblCopertiLiberi.Content = "Il numero di coperti disponibili é: " + Ristorante.CopertiDisponibili();
+                        lblTavoliLiberi.Content = "Tavoli Liberi: " + Ristorante.TavoliLiberi;
+                        lblTavoliPrenotati.Content = "Tavoli prenotati : " + Ristorante.TavoliPrenotati();
+                    }
+                    else
+                    {
+                        lblStato.Content = "Prenotazione Fallita\nsiamo spiacenti";
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Errore nella creazione della prenotazione! verificare i dati inseriti"+ex.Message);
+                }
+                
+            }
+        }
+
+        private void CleanTxt(object sender, RoutedEventArgs e)
+        {
+            if(sender is TextBox)
+            {
+                TextBox txt = (TextBox)sender;
+                txt.Text = "";
+            }
+        }
+    }
+}
